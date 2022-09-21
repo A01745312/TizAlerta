0145ed46cffd1f950c68bb083a1b85f7b42667e8
RC0ndado <81990698+RC0ndado@users.noreply.github.com>
2022-09-21 13:07:13 -0500

    const app express

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// Rutas absolutas
+const path = require('path');
+
+
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+
 // create serverr
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/main',(req)=>{
+    res.sendFile(path.join(__dirname, 'views', 'prueba.html'));
+});
+
+console.log(path.join(__dirname, 'views', 'prueba.html'));
+
+app.listen(8080,()=>console.log("Servidor"));
