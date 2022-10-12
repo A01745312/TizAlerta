b6e7221b058ba2b784ee33a61b2afd9222aceb28
A01745312 <A01745312@itesm.mx>
2022-10-12 14:15:52 -0500

    Ya manda notificaciones

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
+// *********** ARCHIVOS REQUERIDOS **************
 require('dotenv').config()
+const { DBConnect } = require('./config/mysql');
 
+// ******** LIBRERIAS ************
 const express = require('express');
-const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
-const { get } = require('http');
-const { DBConnect } = require('./config/mysql');
+
+
+// ******** CONEXION CON LA SUSCRIPCION DE LA APP *****************
+const FCM = require('fcm-node');
+const serverKey = 'AAAAwJ0hCa4:APA91bEjbMn5zY7xygpQXitNQlQnBaWSPhZ651UATRYooLXnrtKDAcyEsNUPjCAtwB2IQo71vHHByoHuJs9xQkaL87ufx4ScJO9yirNiojIPJPVSoCTo9tGWl2544otDSWmlUGjAIjc9';
+const fcm = new FCM(serverKey);
+
 
 // ********  PUERTO  *********
 const PORT = process.env.PORT || 8080
 
 
-// ******* LLAMA ARCHIVO DE CONEXIÓN CON BD ***********
-//const {DBConnect} = require('./config/mysql')
-
-
 // ********* CREANDO SERVIDOR  **********
 const app = express();
-
-
-
-// *******   MIDDLEWARE  **********
-
-app.use(cors());
-app.use(express.json())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 // ************** VERIFICAR CONEXIÓN CON SERVIDOR  ***************
 
 app.listen(PORT,()=> {
-    console.log("Servidor en línea")
+    console.log("----- ONLINE SERVER -------")
 })
 
 
+// *******   MIDDLEWARE  **********
 
-/* ---------------------------------------------------------------------------------------------------------------  */
+app.use(express.json()) // Para poder leer en formato json
+app.use(express.static(path.join(__dirname, 'public'))) // Declara la ruta estática
+app.use(bodyParser.json()) // Poder leer en formato json
+app.use(bodyParser.urlencoded({extended:true})) // Para poder jalar datos de los html
 
-app.engine('html',require('ejs').renderFile);
-app.set('view engine', 'ejs');
+
+// ************ LEER HTML **********
+
+app.engine('html',require('ejs').renderFile) // Para declarar que se lea las plantillas html con ejs
+app.set('view engine', 'ejs') // Declara ejs como lector de las plantillas
 
 
-// Rutas asignadas
+
+// ************* RUTAS ASIGNADAS **************
 
 app.get('/main',(req, res)=>{
     res.sendFile(path.join(__dirname, 'views', 'prueba.html'));
@@ -57,26 +57,21 @@ app.get('/reportes', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'report.html'));
 });
 
-console.log(path.join(__dirname, 'views', 'prueba.html'));
 
+// ************* FECHA & HORA *************
 
-
-let fecha_hora = new Date();
-const fecha = fecha_hora.getFullYear() + '-' + fecha_hora.getMonth() + '-' + fecha_hora.getDate()
-const hora = fecha_hora.getHours() + ':' + fecha_hora.getMinutes() + ':' + fecha_hora.getSeconds()
-
+let fecha_hora = new Date(); // Crea un objeto fecha
+const fecha = fecha_hora.getFullYear() + '-' + fecha_hora.getMonth() + '-' + fecha_hora.getDate() // Obtiene la fecha por partes
+const hora = fecha_hora.getHours() + ':' + fecha_hora.getMinutes() + ':' + fecha_hora.getSeconds() // Obtiene la hora por partes
 
 
 
 
 /* ------------------ LOGIN -------------------- */
 
-
 app.post("/login", (req, res) => {
     const usuario = req.body.user;
     const contraseña = req.body.password;
-    /* connection.query('SELECT * FROM usuario WHERE Matricula = ?', [usuario] , (error,results) =>{
-    console.log(results);}) */
     if(usuario && contraseña){
         console.log('Empezando la conexión')
         DBConnect.query('SELECT * FROM usuario WHERE Matricula = ?',[usuario], (error,results) =>{
@@ -107,12 +102,6 @@ app.post("/login", (req, res) => {
                 })
                 console.log('Usuario incorrecto');
             }else{
-                    /*console.log(fecha)   // BORRAR
-                    console.log(hora)   // BORRAR
-                    console.log(id_usuario) // BORRAR 
-                    
-                    */
-                    // NO DEJAR LOS CONSOLE LOG
                     DBConnect.query('INSERT INTO entrada SET ?', {
                         id_usuario: id_usuario,
                         fecha: fecha,
@@ -140,17 +129,9 @@ app.post("/reportes", (req,res) => {
     const titulo = req.body.notificacion;
     const descripcion = req.body.descripcion;
     const tipo = req.body.tipo;
-    console.log(titulo, descripcion, tipo, fecha, hora)
-    /*query_id_usuario = connection.query('SELECT * FROM entrada ORDER BY id DESC', (error, results) =>{
-        id_usuario = results[0].id_usuario;
-        console.log(id_usuario)
-    })*/
-    console.log(descripcion)
     if (tipo == "sismo"){
         query_id_entrada = DBConnect.query('SELECT * FROM entrada ORDER BY id DESC', (error, results) => {
             id_entrada = results[0].id;
-            console.log(id_entrada)
-        
         DBConnect.query('INSERT INTO notificacion SET ?', {
             id_suceso: 1,
             id_entrada: id_entrada,
@@ -167,10 +148,129 @@ app.post("/reportes", (req,res) => {
             showConfirmButton: false,
             timer: 1500,
             ruta: 'main'
+        }),
+        DBConnect.query('SELECT * FROM notificacion ORDER BY id DESC', (error, results)=>{
+            const title = results[0].titulo;
+            const desc = results[0].descripcion;
+        
+            const message = {
+        
+                to:'dz_Gc-dZS52337nXTX88sa:APA91bHI3jlKuUcA6OFOmI6YlmblAowiTl13kyFMDLo-ILgtc9j-NWkAyJG310Vqbx6D33VI-7Tv5OjKcElueF1lEseY8feHu1dTWOirBwt1cqGbovCEqLmzaRZeg8II96cddP0hKIoi',
+                notification: {
+                    title: title,
+                    body: desc
+                },
+                data: {
+                    title: 'Título de los datos',
+                    body: '{"name" : "Laptop", "product_id" : "12345", "final_price" : "15300"}'
+                },
+            };
+            
+            fcm.send(message, function (err, response) {
+                if (err) {
+                    console.log("Error al enivar mensaje" + err);
+                    console.log("Respuesta: " + response);
+                } else {
+                    console.log("Mensaje enviado correctamente: ", response);
+                }
+            })
         })
-
     })
-    }
+    } else if (tipo == "inundacion"){
+        query_id_entrada = DBConnect.query('SELECT * FROM entrada ORDER BY id DESC', (error, results) => {
+            id_entrada = results[0].id;
+        DBConnect.query('INSERT INTO notificacion SET ?', {
+            id_suceso: 2,
+            id_entrada: id_entrada,
+            fecha: fecha,
+            hora: hora,
+            titulo: titulo,
+            descripcion: descripcion
+        }),
+        res.render ('report.html' , {
+            alert: true,
+            alertTitle: "Sent",
+            alertMessage: "Successful Welcome!",
+            alertIcon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+            ruta: 'main'
+        }),
+        DBConnect.query('SELECT * FROM notificacion ORDER BY id DESC', (error, results)=>{
+            const title = results[0].titulo;
+            const desc = results[0].descripcion;
+        
+            const message = {
+        
+                to:'dz_Gc-dZS52337nXTX88sa:APA91bHI3jlKuUcA6OFOmI6YlmblAowiTl13kyFMDLo-ILgtc9j-NWkAyJG310Vqbx6D33VI-7Tv5OjKcElueF1lEseY8feHu1dTWOirBwt1cqGbovCEqLmzaRZeg8II96cddP0hKIoi',
+                notification: {
+                    title: title,
+                    body: desc
+                },
+                data: {
+                    title: 'Título de los datos',
+                    body: '{"name" : "Laptop", "product_id" : "12345", "final_price" : "15300"}'
+                },
+            };
+            
+            fcm.send(message, function (err, response) {
+                if (err) {
+                    console.log("Error al enivar mensaje" + err);
+                    console.log("Respuesta: " + response);
+                } else {
+                    console.log("Mensaje enviado correctamente: ", response);
+                }
+            })
+        })
+    })
+    } else if (tipo == "clima"){
+        query_id_entrada = DBConnect.query('SELECT * FROM entrada ORDER BY id DESC', (error, results) => {
+            id_entrada = results[0].id;
+        DBConnect.query('INSERT INTO notificacion SET ?', {
+            id_suceso: 3,
+            id_entrada: id_entrada,
+            fecha: fecha,
+            hora: hora,
+            titulo: titulo,
+            descripcion: descripcion
+        }),
+        res.render ('report.html' , {
+            alert: true,
+            alertTitle: "Sent",
+            alertMessage: "Successful Welcome!",
+            alertIcon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+            ruta: 'main'
+        }),
+        DBConnect.query('SELECT * FROM notificacion ORDER BY id DESC', (error, results)=>{
+            const title = results[0].titulo;
+            const desc = results[0].descripcion;
+        
+            const message = {
+        
+                to:'dz_Gc-dZS52337nXTX88sa:APA91bHI3jlKuUcA6OFOmI6YlmblAowiTl13kyFMDLo-ILgtc9j-NWkAyJG310Vqbx6D33VI-7Tv5OjKcElueF1lEseY8feHu1dTWOirBwt1cqGbovCEqLmzaRZeg8II96cddP0hKIoi',
+                notification: {
+                    title: title,
+                    body: desc
+                },
+                data: {
+                    title: 'Título de los datos',
+                    body: '{"name" : "Laptop", "product_id" : "12345", "final_price" : "15300"}'
+                },
+            };
+            
+            fcm.send(message, function (err, response) {
+                if (err) {
+                    console.log("Error al enivar mensaje" + err);
+                    console.log("Respuesta: " + response);
+                } else {
+                    console.log("Mensaje enviado correctamente: ", response);
+                }
+            })
+        })
+    })
+    } 
 });
 
 
--- config/mysql.js
+++ config/mysql.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql')
-const { createPool } = require('mysql2/promise')
 
 const DBConnect = mysql.createPool({   
         host: "localhost",
         database: 'tizalertap',
         password: '',
-        user: 'root'
-    })
+        user: 'root',
+    },
+    console.log('******** DATABASE CONNECTION ********** '))
 
-console.log('CONEXIÓN DATABASE')
 
+/*
 DBConnect.query('SELECT * FROM usuario', (err, results) => {
     console.log(' ***** Primer registro desde archivo  "mysql.js" *****\n', results[0], '\n Termina archivo mysql.js \n' )
-})
+})*/
 
 module.exports = {DBConnect}
 
